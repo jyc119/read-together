7f820a9e5cf9936f924d68ae21a441f9a3e1be5c
Jordan Chin <jc9627@g.rit.edu>
2025-01-06 08:55:15 +0800

    added packages

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{js,jsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#F5F5DC",
+        secondary: "#a032a8",
+      },
+    },
+  },
+  plugins: [],
+};
